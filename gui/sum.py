6f99d8f0ba008cfbdb7427dd7ddcf83b246a6fbb
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-09-26 19:06:44 +0200

    h

---
+++ gui/sum.py
@@ -0,0 +1,9 @@
+sum(iterrable):
+if (sum(1, 2) == 3 and
+    sum(0, -100) == -100 and
+    sum(-1, -2) == -3):
+
+    # Comment if all cases fine:
+    print("It works!")
+else:
+    print("Something's wrong!")
