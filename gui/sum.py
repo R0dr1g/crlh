66d45e3356eef95ec081aa7a8d1d803e087b337f
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-09-27 15:16:22 +0200

    ggreg

---
+++ gui/sum.py
@@ -0,0 +1,10 @@
+def sum(a, b):
+    return a+b
+if (sum(1, 2) == 3 and
+    sum(0, -100) == -100 and
+    sum(-1, -2) == -3):
+
+    
+    print("It works!")
+else:
+    print("Something's wrong!")
