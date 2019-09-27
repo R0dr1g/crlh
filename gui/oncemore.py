e134428891ed12d2aabd04aeddd22b6bb3b8e52f
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-09-27 20:55:08 +0200

    lm

---
+++ gui/oncemore.py
@@ -0,0 +1,22 @@
+def gui(message, number):
+    print()
+    for i in range(number):
+        str(i+1)
+        print(message)
+    print()
+
+finish = False
+while finish == False:
+    print()
+    print("diz gui crlh")
+    message = input()
+    print("diz um numero crlh")
+    try:
+        number = int(input())
+        gui(message, number)
+    except ValueError:
+        print("Isso n é numero crlho")
+
+    g = input(Queres continuar crlh (ye/nah nigga))
+    if (m != "ye"):
+        finish = True
