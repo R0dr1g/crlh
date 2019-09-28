4a8c69182ebd2078e45d09817777968db68d8cde
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-09-28 12:14:10 +0200

    fe

--- gui/continue.py
+++ gui/continue.py
@@ -2,9 +2,9 @@ print("Do you want to continue (yes/no)")
 
 answer = input()
 
-if (answer !="yes"):
-    finish = True
 if (answer !="No"):
+    finish = True
+if (answer !="Yes"):
     finish = False
 else:
     print("I don't understand you, sorry!")
