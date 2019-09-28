78421bd390b66dc0249a2ca4ba9acda2c01dd23c
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-09-28 12:28:29 +0200

    e

--- gui/continue.py
+++ gui/continue.py
@@ -3,10 +3,11 @@ while finish == False:
     print ("Do you want to continue ?? (Yes/No)")
 
     m = input()
-    
+
     if (m !="Yes"):
         finish = False
     if(m !="No"):
         finish = True
     else:
         print("I don't understand you, sorry!")
+        finish = False
