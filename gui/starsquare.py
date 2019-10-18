167b947829ca5ce70179d2d66dede98a21092b06
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-10-18 14:54:17 +0200

    f

--- gui/starsquare.py
+++ gui/starsquare.py
@@ -2,9 +2,8 @@ def starsquare(n):
     for i in range(n):
         str(i+1)
         star = ""
-        print(star, "* ")
-        print()
-        print("* ")
+        print(star, "*", i+1)
+        print("*", 1+1)
 
 
 print("How many stars do you want on each side?")
