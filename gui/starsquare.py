a730c27f70e9199edb28edb07542eb5a5524fe05
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-10-18 15:03:05 +0200

    de

--- gui/starsquare.py
+++ gui/starsquare.py
@@ -1,15 +1,18 @@
 def starsquare(n):
     star = ""
     for i in range(n):
+        i^2
         star += "* "
-        print("*")
+
     return star
 
 
 print("How many stars do you want on each side?")
-print()
+
 try:
     n = int(input())
+    print()
     print(starsquare(n))
+
 except ValueError:
     print("Not a number!")
