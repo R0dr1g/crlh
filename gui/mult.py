2f3d77204122bb071095e449c0a11841fc3b2fa8
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-11-08 15:18:20 +0100

    h

--- gui/mult.py
+++ gui/mult.py
@@ -1,7 +1,24 @@
 
 def randmult(number, ceiling):
     import random
+    randomult = ""
     for i in range(number):
+        a = str(random.randint(0, ceiling))
+        b = str(random.randint(0, ceiling))
+        randquest = a + " x " + b
+    return randquest
+    if
+
 
 finish
-while (finish == False):
+while finish == False:
+    try:
+        print()
+        print("How many mults do you want to generate?")
+        number = int(input())
+        print()
+        print("Limit :")
+        ceiling = int(input())
+        randmult(number, ceiling)
+    except ValueError:
+        print("Not a number !")
