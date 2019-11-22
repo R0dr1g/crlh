3ad774173c715c1c9bf5e86634130dfd0316dcd4
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-11-22 15:18:38 +0100

    t

--- gui/mult.py
+++ gui/mult.py
@@ -9,19 +9,27 @@ try:
     def randmult(a, b):
         randquest = ("How much is " + str(a) + " x " + str(b) + "?")
         return randquest
-
+    answer = list()
+    correction = list()
     for q in range(number):
         import random
         a = int((random.randint(0, ceiling)))
         b = int((random.randint(0, ceiling)))
         print(randmult(a, b))
-        answer = list()
         answer.append(int(input()))
-        correction = list()
         correction.append(a*b)
 
-    def allansw(a, b, c)):
-        for i in range(c):
-            correct = ("Q" + str(i+1) + )
+    def corasnw():
+        correct = ("Q" + str(i+1) + ") " + str(a) + " x " + str(b) + " User answered " + str(answer[i]) + ". The answer was correct!")
+        return correct
+    def wrongans():
+        wrong = ("Q" + str(i+1) + ") " + randmult(a, b) + " User answered " + str(answer[i]) + ". The answer was wrong.")
+        return wrong
+    print()
+    for i in range(number):
+        if (answer == correction):
+            print(coransw())
+        if (answer != correction):
+            print(wrongans())
 except ValueError:
     print("Not a number !")
