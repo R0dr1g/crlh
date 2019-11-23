4692a55199361224590c0b1d77d67f168e4a6b70
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-11-23 21:07:02 +0100

    jhto

---
+++ gui/sort.py
@@ -0,0 +1,20 @@
+try:
+    import random
+    print()
+    print("How many numbers u want?")
+    lenght = int(input())
+    print()
+    print("Maximum :")
+    ceiling =  int(input())
+    sortlist = list()
+    for i in range(lenght):
+        sortlist.append(random.randint(0, ceiling))
+    print()
+    print("Original list: " + str(sortlist))
+    print()
+    def sortint():
+        sortlist.sort()
+        return sortlist
+    print("Here is the list sorted in ascending order :" + str(sortint()))
+except ValueError:
+    print("Not an integer!")
