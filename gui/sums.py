f9d235152b810fee12b891b497b4134efa0f5cc7
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-11-24 17:12:16 +0100

    junio

---
+++ gui/sums.py
@@ -0,0 +1,45 @@
+try:
+    print()
+    print("Number of questions :")
+    number = int(input())
+    print()
+    print("Ceiling :")
+    ceiling = int(input())
+    print()
+    yyy = list()
+    for i in range(number):
+        def randmult(a, b):
+            randquest = ("How much is " + str(a) + " + " + str(b) + "?")
+            yyy.append(randquest)
+            return randquest
+    answer = list()
+    correction = list()
+    for q in range(number):
+        import random
+        a = int((random.randint(0, ceiling)))
+        b = int((random.randint(0, ceiling)))
+        print(randmult(a, b))
+        answer.append(int(input()))
+        correction.append(a+b)
+    counter = 0
+    def coransw(a, b):
+        correct = ("Q" + str(i+1) + ") " + str(yyy[i]) + " User answered " + str(answer[i]) + ". The answer was correct!")
+        return correct
+    def wrongans(a, b):
+        wrong = ("Q" + str(i+1) + ") " + str(yyy[i]) + " User answered " + str(answer[i]) + ". The answer was wrong.")
+        return wrong
+    print()
+    for i in range(number):
+        if (answer[i] == correction[i]):
+            print(coransw(a, b))
+            counter +=1
+        if (answer[i] != correction[i]):
+            print(wrongans(a, b))
+
+    def score():
+        zzz = (str(counter) + "/" + str(number))
+        return zzz
+    print()
+    print(score())
+except ValueError:
+    print("Not a number !")
---
+++ gui/tests.py
@@ -0,0 +1,12 @@
+print("Hello ! Welcome to the testing board, where you can choose the kind of test you're gonna be doing!")
+print()
+print("So, what test do you want to take?? (Multiplication / Division / Subtraction / Addition)")
+testt = input()
+if (testt == "Division"):
+    import divis
+if (testt == "Multiplication"):
+    import mult
+if (testt == "Subtraction"):
+    import sub
+if (testt == "Addition"):
+    import sums
