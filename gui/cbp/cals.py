e5c2e31ae54f4dda288a3fbd9b790e9a5f4884ed
R0dr1g <35218888+R0dr1g@users.noreply.github.com>
2019-12-05 11:58:37 +0100

    §

--- gui/cbp/cals.py
+++ gui/cbp/cals.py
@@ -1,3 +1,4 @@
+print()
 class Student:
     def __init__(self, name, cless, age):
         self.name = str(name)
@@ -5,11 +6,11 @@ class Student:
         self.age = int(age)
 
     def present(self):
-         print("Hello, my name is {}, i'm in class {} and i'm {} years old!") .\
-        format(self.name, self.cless, self.age)
+         print("Hello, my name is " + str(self.name) + " i'm in class " + str(self.cless) + " and i'm " + str(self.age) + " years old!")
 
     def changeage(self):
-        b
+        return self.age + 1
+
 s1 = Student("Jerry", "S4Pta",13)
 s2 = Student("Richard", "S5Nea",14)
 s3 = Student("Gilberto", "S7Fia",18)
@@ -21,4 +22,8 @@ s8 = Student("Rodrigo", "S5Fra",15)
 s9 = Student("David", "S7Frb",18)
 s10 = Student("Guilherme", "S3Lit",17)
 
-s1.present()
+print("Student : (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)")
+nem = int(input())
+fu12 = list()
+for i in range(nem):
+    fu12.append(s[i+1].present())
